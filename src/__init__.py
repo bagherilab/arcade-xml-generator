4ae29b25aa3061d39c38686e8bb75ba42277d5c9
Olaf Corning <olafc@uw.edu>
2023-12-19 23:48:20 -0800

    add init files

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
-from src.ObjectToXML import xml_object
+from src.xml_object import XMLObject
+from src.read_input_xml import ReadInputFunctions, ReadInputModuleSpecific
+from src.compile_xml import ReadOutputFunctions
+from src.gui import Window
